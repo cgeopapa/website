53cee935e39bd44776ae58e386669aab85f7dfa3
cgeopapa <cgeocodgod@gmail.com>
2021-03-07 19:49:21 +0200

    Mobile responsive design

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +6,11 @@ import { Component, NgModule, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  public isMobile: boolean = false;
 
-  constructor() { }
+  constructor() {
+    this.isMobile = window.innerWidth < 750;
+  }
 
   ngOnInit(): void {
   }
